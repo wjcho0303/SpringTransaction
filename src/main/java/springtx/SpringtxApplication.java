95ed660a0a4ce719ffba4a9c21e575bed5791491
wjcho <wj930303@gmail.com>
2024-08-07 22:52:35 +0900

    Initial commit

---
+++ src/main/java/springtx/SpringtxApplication.java
@@ -0,0 +1,12 @@
+package springtx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtxApplication.class, args);
+    }
+}
