37c612cecdc424c60eb7810b3db94da06379a057
wjcho <wj930303@gmail.com>
2024-08-08 19:15:52 +0900

    Feat: 트랜잭션 학습 내용

---
+++ src/test/java/springtx/SpringtxApplicationTests.java
@@ -0,0 +1,12 @@
+package springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringtxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
