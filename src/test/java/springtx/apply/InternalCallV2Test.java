f46deed93c72b52dba81cdfc96eb9a076d59ab3f
wjcho <wj930303@gmail.com>
2024-08-09 21:39:36 +0900

    Feat: 내부 호출 트랜잭션 미적용 문제 해결 예제(클래스 분리)

---
+++ src/test/java/springtx/apply/InternalCallV2Test.java
@@ -0,0 +1,83 @@
+package springtx.apply;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+@Slf4j
+public class InternalCallV2Test {
+    @Autowired
+    CallService callService;
+
+    @Autowired
+    InternalService internalService;
+
+    @Test
+    void printProxyV2() {
+        log.info("call Service class = {}", callService.getClass());
+    }
+
+    @Test
+    void internalCallV2() {
+        internalService.internal();
+    }
+
+    @Test
+    void externalCallV2() {
+        callService.external();
+    }
+
+
+    @TestConfiguration
+    static class InternalCallV2Config {
+
+        @Bean
+        CallService callService() {
+            return new CallService(internalService());
+        }
+
+        @Bean
+        InternalService internalService() {
+            return new InternalService();
+        }
+    }
+
+
+    @RequiredArgsConstructor
+    static class CallService {
+
+        private final InternalService internalService;
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internalService.internal(); // external 메서드에서 CallService 의 내부 메서드인 internal 메서드를 호출
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active = {}", txActive);
+        }
+    }
+
+    static class InternalService {
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active = {}", txActive);
+        }
+    }
+}
