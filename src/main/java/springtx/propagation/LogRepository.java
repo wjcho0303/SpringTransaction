c54294036aa7a9f1aecbe78c56e0cb1dc77dc364
wjcho <wj930303@gmail.com>
2024-08-12 00:07:32 +0900

    Feat: 트랜잭션 전파 예제를 위한 코드

---
+++ src/main/java/springtx/propagation/LogRepository.java
@@ -0,0 +1,35 @@
+package springtx.propagation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
+public class LogRepository {
+
+    private final EntityManager em;
+
+    @Transactional
+    public void save(Log logMessage) {
+        log.info("log 저장");
+        em.persist(logMessage);
+
+        if (logMessage.getMessage().contains("로그예외")) {
+            log.info("log 저장 시 예외 발생");
+            throw new RuntimeException("예외 발생");
+        }
+    }
+
+    public Optional<Log> findByMessage(String message) {
+        return em.createQuery("select l from Log l where l.message = :message", Log.class)
+                .setParameter("message", message)
+                .getResultList().stream().findAny();
+    }
+
+}
---
+++ src/main/java/springtx/propagation/MemberService.java
@@ -0,0 +1,55 @@
+package springtx.propagation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final LogRepository logRepository;
+
+    /**
+     * 회원과 DB 로그를 함께 남기는 비즈니스 로직
+     * 현재 별도의 트랜잭션은 설정하지 않는다.
+     * @param username
+     */
+    public void joinV1(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        logRepository.save(logMessage);
+        log.info("== logRepository 호출 종료 ==");
+    }
+
+    /**
+     * joinV1()과 같은 기능을 수행
+     * DB 로그 저장 시 예외가 발생하면 예외를 복구한다.
+     * 현재 별도의 트랜잭션은 설정하지 않는다.
+     * @param username
+     */
+    public void joinV2(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        try {
+            logRepository.save(logMessage);
+        } catch (RuntimeException e) {
+            log.info("log 저장에 실패했습니다. logMessage = {}", logMessage.getMessage());
+            log.info("정상 흐름 변환");
+        }
+        log.info("== logRepository 호출 종료 ==");
+    }
+}
